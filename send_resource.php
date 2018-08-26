3fa67806fcfb6d218b61addcccba28e64525d5ec
Colin Matcham <cmatcham@gmail.com>
2018-08-27 08:54:38 +1200

    update send resource

--- send_resource.php
+++ send_resource.php
@@ -47,10 +47,10 @@
           //send email to us
 
           $subject = "TAG The Agency Clip Guide";
-          $body = "New request for clip guide";
+          $body = "New request for clip guide\n\n";
           $body .= "From: $sender_email";
-          $email_headers = "From: TAG The Agency <play@tagtheagency.com>";
-          mail("play@tagtheagency.com", $subject, $body, $email_headers);
+          $email_headers = "From: TAG The Agency <colin@tagtheagency.com>";
+          mail("colin@tagtheagency.com", $subject, $body, $email_headers);
 
           echo '{"status": "success"}';
           exit;
